1c0962da6bea3d6c655e2c7fce4ad5239551e0e6
Elena <elenamitchenko@yandex.ru>
2020-09-03 00:30:49 +0300

    Улучшаем скорость загрузки

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const rename = require("gulp-rename");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const svgstore = require("gulp-svgstore");
+const terser = require("gulp-terser");
 const del = require("del");
 
 
@@ -32,6 +33,17 @@ const styles = () => {
 
 exports.styles = styles;
 
+//Script
+
+const script = () => {
+  return gulp.src("source/js/*.js")
+    .pipe(terser())
+    .pipe(rename({suffix: ".min"}))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+exports.script = script;
+
 // Images
 
 const images = () => {
@@ -53,6 +65,8 @@ const createWebp = () => {
     .pipe(gulp.dest("build/img"))
 }
 exports.webp = createWebp;
+
+// Sprite
 
 const sprite = () => {
   return gulp.src("source/img/**/icons-*.svg")
@@ -91,7 +105,6 @@ const copy = () => {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
     "source/*.ico"
   ], {
     base: "source"
@@ -120,6 +133,7 @@ const build = gulp.series(
   createWebp,
   copy,
   styles,
+  script,
   sprite,
   html
 );
@@ -130,7 +144,7 @@ exports.build = build;
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", gulp.series("html"));
-  gulp.watch("source/js/*.js").on("change", gulp.series("copy"));
+  gulp.watch("source/js/*.js").on("change", gulp.series("script"));
 }
 
 exports.default = gulp.series(
